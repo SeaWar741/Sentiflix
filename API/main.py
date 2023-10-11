b84745d9cfb8718b781aeedec9932663396a6ed7
SeaWar741 <juanca741@gmail.com>
2023-10-11 15:11:39 -0600

    Track large file with Git LFS

---
+++ API/main.py
@@ -0,0 +1,30 @@
+# Create Flask app
+import json
+import pandas as pd
+import numpy as np
+import certifi
+import jwt
+import datetime
+from bson import ObjectId
+from functools import wraps
+import simplejson
+
+import hashlib, binascii, os
+
+from flask import Flask, jsonify, request
+
+from flask_cors import CORS, cross_origin
+
+
+app = Flask(__name__)
+
+
+@app.route('/api', methods=['GET'])
+@cross_origin()
+def api():
+    return jsonify({'message': 'Hello, World!'})
+
+
+# Run Server on port 5000
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',debug=True)
