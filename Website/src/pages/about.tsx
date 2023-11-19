e071e1dbcf50bd869a52d75da78b19a67e62c776
rmp0 <135342595+rmp0@users.noreply.github.com>
2023-11-18 18:39:15 -0600

    Update added person about.tsx

--- Website/src/pages/about.tsx
+++ Website/src/pages/about.tsx
@@ -9,6 +9,14 @@ const people = [
     twitterUrl: "#",
     linkedinUrl: "#",
   },
+   {
+    name: "Rodrigo Montelongo Pinales",
+    role: "Front-end Developer",
+    imageUrl:
+      "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+    twitterUrl: "https://www.instagram.com/ro_montelongo0/",
+    linkedinUrl: "https://www.linkedin.com/in/rodrigo-montelongo/",
+  },
 ];
 
 export default function Page() {
